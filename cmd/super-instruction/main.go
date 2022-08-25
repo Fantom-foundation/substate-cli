025a9331fe429d78265823a92f05e7457f54c246
Bernhard Scholz <bernhard@fantom.foundation>
2022-08-25 14:41:57 +1000

    Add basic block write function

--- cmd/super-instruction/main.go
+++ cmd/super-instruction/main.go
@@ -22,6 +22,7 @@ func readContracts() {
 		log.Fatal("Cannot open codedb!")
 	}
 	defer db.Close()
+	// read all contracts and populate CodeRegistry map
 	iter := db.NewIterator(nil, nil)
 	ctr := 0;
 	for iter.Next() {
@@ -65,6 +66,59 @@ func readJumpDestFrequency() {
 	fmt.Printf("Read %v JUMPDEST frequencies.\n", ctr)
 }
 
+func writeBasicBlocks() {
+	// open sqlite3 database
+	db, err := sql.Open("sqlite3", "./basicblock.db") // Open the created SQLite File
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	defer db.Close()
+
+	// drop jump-dest table
+	const dropBasicBlock string = `DROP TABLE IF EXISTS BasicBlock;`
+	statement, err := db.Prepare(dropBasicBlock)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	_, err = statement.Exec()
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+
+	// create new table
+	const createBasicBlock string = `
+	CREATE TABLE BasicBlock (
+	 contract TEXT, 
+	 pc NUMERIC,
+	 frequency NUMERIC,
+	 instructions BLOB
+	);`
+	statement, err = db.Prepare(createBasicBlock)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	_, err = statement.Exec()
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+
+	// populate values
+	insertFrequency := `INSERT INTO BasicBlock(contract, pc, frequency, instructions) VALUES (?, ?, ?, ?)`
+	statement, err = db.Prepare(insertFrequency)
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+	for contract, freqMap := range JumpDestFrequency {
+		for pc, freq := range freqMap {
+                        instructions := [] byte{1,2}
+			_, err = statement.Exec(contract.String(), pc, freq, instructions) 
+			if err != nil { 
+				 log.Fatalln(err.Error())
+			}
+		}
+	}
+	
+}
 
 func main() {
 	fmt.Printf("Read contracts database ...\n")
@@ -72,5 +126,9 @@ func main() {
 
 	fmt.Printf("Read JUMPDEST frequencies database ...\n")
 	readJumpDestFrequency()
+
+	fmt.Printf("Write basic blocks ...\n")
+	writeBasicBlocks()
+
 	fmt.Printf("Done.\n")
 }
