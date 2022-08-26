e35f6118365c37fadf258eef783332c70a2a07ea
Bernhard Scholz <Bernhard.Scholz@sydney.edu.au>
2022-08-26 11:58:27 +1000

    Fixed basic-block writing

--- cmd/super-instruction/main.go
+++ cmd/super-instruction/main.go
@@ -11,19 +11,20 @@ import (
 )
 
 var (
-	Code              map[common.Address][]byte            = make(map[common.Address][]byte)
-	JumpDestFrequency map[common.Address]map[uint64]uint64 = make(map[common.Address]map[uint64]uint64)
-	ContractDB        string                               = "./contracts.db"
+	Code                     = map[common.Address][]byte{}
+	JumpDestFrequency        = map[common.Address]map[uint64]uint64{}
+	ContractDB        string = "./contracts.db"
 )
 
-// read from contract db to populate the code registry
+// read from contract db to populate the map for contracts
 func readContracts() {
+	// open leveldb database
 	db, err := leveldb.OpenFile(ContractDB, nil)
 	if err != nil {
 		log.Fatal("Cannot open codedb!")
 	}
 	defer db.Close()
-	// read all contracts and populate Code map
+	// read all contracts and populate map
 	iter := db.NewIterator(nil, nil)
 	ctr := 0
 	for iter.Next() {
@@ -35,9 +36,10 @@ func readContracts() {
 	fmt.Printf("Read %v contracts.\n", ctr)
 }
 
+// read the JUMPDEST frequency database
 func readJumpDestFrequency() {
 	// open sqlite3 database
-	db, err := sql.Open("sqlite3", "./jumpdest.db") // Open the created SQLite File
+	db, err := sql.Open("sqlite3", "./jumpdest.db")
 	if err != nil {
 		log.Fatal(err.Error())
 	}
@@ -67,6 +69,7 @@ func readJumpDestFrequency() {
 	fmt.Printf("Read %v JUMPDEST frequencies.\n", ctr)
 }
 
+// write basic blocks into a sqlite3 database
 func writeBasicBlocks() {
 	// open sqlite3 database
 	db, err := sql.Open("sqlite3", "./basicblock.db") // Open the created SQLite File
@@ -83,10 +86,10 @@ func writeBasicBlocks() {
 	}
 	_, err = statement.Exec()
 	if err != nil {
-		log.Fatalln(err.Error())
+		log.Fatal(err.Error())
 	}
 
-	// create new table
+	// create new basic block table
 	const createBasicBlock string = `
 	CREATE TABLE BasicBlock (
 	 contract TEXT, 
@@ -100,30 +103,44 @@ func writeBasicBlocks() {
 	}
 	_, err = statement.Exec()
 	if err != nil {
-		log.Fatalln(err.Error())
+		log.Fatal(err.Error())
 	}
 
 	// populate values
 	insertFrequency := `INSERT INTO BasicBlock(contract, pc, frequency, instructions) VALUES (?, ?, ?, ?)`
 	statement, err = db.Prepare(insertFrequency)
 	if err != nil {
-		log.Fatalln(err.Error())
+		log.Fatal(err.Error())
 	}
 	for contract, freqMap := range JumpDestFrequency {
 		for start, freq := range freqMap {
 			pc := start
-			instructions := []OpCode{}
+			instructions := []vm.OpCode{}
 			length := uint64(len(Code[contract]))
 			for {
+
+				// exceed code segment
 				if pc >= length {
 					break
 				}
+
+				// fetch op-code
 				op := vm.OpCode(Code[contract][pc])
 				instructions = append(instructions, op)
 
-                // push operation
-				if op >= core.vm.PUSH1 && op <= core.vm.PUSH32 {
-					numbits := op - core.vm.PUSH1 + 1
+				// end of basic block?
+				if op == vm.JUMP ||
+					op == vm.JUMPI ||
+					op == vm.STOP ||
+					op == vm.RETURN ||
+					op == vm.REVERT ||
+					op == vm.SELFDESTRUCT {
+					break
+				}
+
+				// skip constant of a push operation
+				if op >= vm.PUSH1 && op <= vm.PUSH32 {
+					numbits := op - vm.PUSH1 + 1
 					if numbits >= 8 {
 						for ; numbits >= 16; numbits -= 16 {
 							pc += 16
@@ -149,15 +166,16 @@ func writeBasicBlocks() {
 						pc += 7
 					}
 				}
+
+				// skip to next instruction
 				pc++
 			}
 			_, err = statement.Exec(contract.String(), pc, freq, instructions)
 			if err != nil {
-				log.Fatalln(err.Error())
+				log.Fatal(err.Error())
 			}
 		}
 	}
-
 }
 
 func main() {
