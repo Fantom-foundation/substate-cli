1638158cca73917a181267e30c7944dfeaaf45ba
Herbert Jordan <herbert@swrnd.ch>
2022-09-02 13:56:30 +0200

    adds access list support to fast state DB implementation

--- cmd/substate-cli/replay/in_memory_state_db.go
+++ cmd/substate-cli/replay/in_memory_state_db.go
@@ -44,14 +44,16 @@ type snapshot struct {
 	parent *snapshot
 	id     int
 
-	touched  map[common.Address]int // Set of referenced accounts
-	balances map[common.Address]*big.Int
-	nonces   map[common.Address]uint64
-	codes    map[common.Address][]byte
-	suicided map[common.Address]int // Set of destructed accounts
-	storage  map[slot]common.Hash
-	logs     []*types.Log
-	refund   uint64
+	touched           map[common.Address]int // Set of referenced accounts
+	balances          map[common.Address]*big.Int
+	nonces            map[common.Address]uint64
+	codes             map[common.Address][]byte
+	suicided          map[common.Address]int // Set of destructed accounts
+	storage           map[slot]common.Hash
+	accessed_accounts map[common.Address]int
+	accessed_slots    map[slot]int
+	logs              []*types.Log
+	refund            uint64
 }
 
 func makeSnapshot(parent *snapshot, id int) *snapshot {
@@ -60,16 +62,18 @@ func makeSnapshot(parent *snapshot, id int) *snapshot {
 		refund = parent.refund
 	}
 	return &snapshot{
-		parent:   parent,
-		id:       id,
-		touched:  map[common.Address]int{},
-		balances: map[common.Address]*big.Int{},
-		nonces:   map[common.Address]uint64{},
-		codes:    map[common.Address][]byte{},
-		suicided: map[common.Address]int{},
-		storage:  map[slot]common.Hash{},
-		logs:     make([]*types.Log, 0),
-		refund:   refund,
+		parent:            parent,
+		id:                id,
+		touched:           map[common.Address]int{},
+		balances:          map[common.Address]*big.Int{},
+		nonces:            map[common.Address]uint64{},
+		codes:             map[common.Address][]byte{},
+		suicided:          map[common.Address]int{},
+		storage:           map[slot]common.Hash{},
+		accessed_accounts: map[common.Address]int{},
+		accessed_slots:    map[slot]int{},
+		logs:              make([]*types.Log, 0),
+		refund:            refund,
 	}
 }
 
@@ -229,47 +233,57 @@ func (db *inMemoryStateDB) Empty(addr common.Address) bool {
 }
 
 func (db *inMemoryStateDB) PrepareAccessList(sender common.Address, dest *common.Address, precompiles []common.Address, txAccesses types.AccessList) {
-	// ignored
-	panic("not implemented")
+	db.AddAddressToAccessList(sender)
+	if dest != nil {
+		db.AddAddressToAccessList(*dest)
+		// If it's a create-tx, the destination will be added inside evm.create
+	}
+	for _, addr := range precompiles {
+		db.AddAddressToAccessList(addr)
+	}
+	for _, el := range txAccesses {
+		db.AddAddressToAccessList(el.Address)
+		for _, key := range el.StorageKeys {
+			db.AddSlotToAccessList(el.Address, key)
+		}
+	}
 }
 func (db *inMemoryStateDB) AddressInAccessList(addr common.Address) bool {
-	// ignored
-	panic("not implemented")
+	for state := db.state; state != nil; state = state.parent {
+		if _, present := state.accessed_accounts[addr]; present {
+			return true
+		}
+	}
+	return false
 }
-func (db *inMemoryStateDB) SlotInAccessList(addr common.Address, slot common.Hash) (addressOk bool, slotOk bool) {
-	// ignored
-	panic("not implemented")
+func (db *inMemoryStateDB) SlotInAccessList(addr common.Address, key common.Hash) (addressOk bool, slotOk bool) {
+	addressOk = db.AddressInAccessList(addr)
+	id := slot{addr, key}
+	for state := db.state; state != nil; state = state.parent {
+		if _, present := state.accessed_slots[id]; present {
+			slotOk = true
+			return
+		}
+	}
+	return
 }
 
 func (db *inMemoryStateDB) AddAddressToAccessList(addr common.Address) {
-	// ignored
-	panic("not implemented")
+	db.state.accessed_accounts[addr] = 0
 }
 
-func (db *inMemoryStateDB) AddSlotToAccessList(addr common.Address, slot common.Hash) {
-	// ignored
-	panic("not implemented")
+func (db *inMemoryStateDB) AddSlotToAccessList(addr common.Address, key common.Hash) {
+	db.AddAddressToAccessList(addr)
+	db.state.accessed_slots[slot{addr, key}] = 0
 }
 
 func (db *inMemoryStateDB) RevertToSnapshot(id int) {
-	//accounts := map[common.Address]int{}
 	for ; db.state != nil && db.state.id != id; db.state = db.state.parent {
 		// nothing
-		/*
-			for addr, value := range db.state.balances {
-				fmt.Printf("  removing balance of %v which is %v\n", addr, value)
-				accounts[addr] = 0
-			}
-		*/
 	}
 	if db.state == nil {
 		panic(fmt.Errorf("unable to revert to snapshot %d", id))
 	}
-	/*
-		for addr := range accounts {
-			fmt.Printf("Reverted balance of %v to %v\n", addr, db.GetBalance(addr))
-		}
-	*/
 }
 
 func (db *inMemoryStateDB) Snapshot() int {
