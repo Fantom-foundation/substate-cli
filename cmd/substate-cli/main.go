49d46c583bc99b0ea8ef4fc2d4c5ce521602a6b2
wsodsong <wasuwee.sodsong@gmail.com>
2022-12-27 07:20:43 +0000

    Record deleted accounts and resurrected accounts
    This PR is a part of a fix which clears storage of suicided contracts.
    Since substate only keeps touched addresses, it cannot distingush between
    storage keys have been deleted or they are not accessed. For simplicity,
    a storage slot is marked as deleted by setting to common.Hash{}. This change
    is particularly important for accounts which later resurrected.
    
    Recording of deleted addresses and resurrected addresses
    - deleted address: an account which was successfully suicided in a transaction.
    - resurrected address: an account created in a transaction which was
    previously suicided.
    - only successful transactions are recorded.

--- cmd/substate-cli/main.go
+++ cmd/substate-cli/main.go
@@ -4,10 +4,10 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/ethereum/go-ethereum/substate"
-	"github.com/ethereum/go-ethereum/params"
 	"github.com/Fantom-foundation/substate-cli/cmd/substate-cli/db"
 	"github.com/Fantom-foundation/substate-cli/cmd/substate-cli/replay"
+	"github.com/ethereum/go-ethereum/params"
+	"github.com/ethereum/go-ethereum/substate"
 	"github.com/urfave/cli/v2"
 )
 
@@ -30,13 +30,14 @@ var (
 
 func main() {
 	app := &cli.App{
-		Name:		"Substate CLI Manger",
-		HelpName:	"substate-cli",
-		Version:	params.VersionWithCommit(gitCommit, gitDate),
-		Copyright:	"(c) 2022 Fantom Foundation",
-		Flags:		[]cli.Flag{},
-		Commands:	[]*cli.Command{
+		Name:      "Substate CLI Manger",
+		HelpName:  "substate-cli",
+		Version:   params.VersionWithCommit(gitCommit, gitDate),
+		Copyright: "(c) 2022 Fantom Foundation",
+		Flags:     []cli.Flag{},
+		Commands: []*cli.Command{
 			&replay.ReplayCommand,
+			&replay.GetDeletedAccountsCommand,
 			&replay.GetStorageUpdateSizeCommand,
 			&replay.GetCodeCommand,
 			&replay.GetCodeSizeCommand,
